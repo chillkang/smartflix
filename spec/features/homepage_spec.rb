09c1c964d213024632bc01e54707ec2ca0d1c0c3
chillkang <seulkee.kang@smartpension.co.uk>
2022-09-08 15:52:39 +0100

    make changes to spec, routes, and gemfile

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,7 +1,7 @@
 require "rails_helper"
 
 feature "homepage" do
-  scenario "enables me to view 'smartflix'" do
+  scenario "A user visits the homepage and views 'smartflix'" do
     visit "/"
     expect(page).to have_content 'smartflix'
   end
